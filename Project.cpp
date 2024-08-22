0edc66fd6d5b5a9cd476ba266a3ef198738d5c4a
guille-011 <apontecardenas54321@gmail.com>
2024-08-22 12:28:09 -0500

    Cambionose

--- Project.cpp
+++ Project.cpp
@@ -3,7 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "HOla xdddxx";
+	cout << ":VVVVVVVVVVV";
 	//adasdada
 	//pan
 	return 0;
